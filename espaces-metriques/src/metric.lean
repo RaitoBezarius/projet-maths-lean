b9ca4d9865e47c29a54529865d25b9ad9eaa8bd9
Raito Bezarius <masterancpp@gmail.com>
2020-03-20 16:29:50 +0100

    espace_metrique: mise à jour du fichier, preuve de BW énormément avancée

--- espaces-metriques/src/metric.lean
+++ espaces-metriques/src/metric.lean
@@ -3,6 +3,7 @@ import data.real.basic
 import data.real.cau_seq
 import data.set
 import order.bounds
+import order.complete_lattice
 import topology.algebra.ordered
 
 noncomputable theory
@@ -47,8 +48,8 @@ open espace_metrique
 instance real.metric_space : espace_metrique ℝ :=
 { d                  := λx y, abs (x - y),
   d_pos              := by simp [abs_nonneg],
-  presep             := by simp [add_neg_eq_zero],
-  sep                := by simp [add_neg_eq_zero],
+  presep             := begin simp, apply sub_eq_zero_of_eq end,
+  sep                := begin simp, apply eq_of_sub_eq_zero end,
   sym                := assume x y, abs_sub _ _,
   triangle           := assume x y z, abs_sub_le _ _ _ }
 
@@ -89,10 +90,40 @@ def sous_suite (x: ℕ → X) (φ : ℕ → ℕ) (n: ℕ) := x (φ n)
 
 -- point limite
 def point_limite (S: set X) (l: X) := ∃ (x : ℕ → X), (∀ n : ℕ, x n ∈ S ∧ x n ≠ l) ∧ (converge x l)
+
+-- sous hypothèse que le sup ou l'inf ne sont pas dans l'ensemble, ils forment des points limites.
+-- niveau: facile
+lemma sup_est_un_point_limite [complete_linear_order X] {S: set X}: 
+  complete_lattice.Sup S ∉ S → point_limite S (complete_lattice.Sup S) := sorry
+lemma inf_est_un_point_limite [complete_linear_order X] (S: set X): 
+  complete_lattice.Inf S ∉ S → point_limite S (complete_lattice.Inf S) := sorry
+
 -- valeur d'adhérence.
 def adhere (x: ℕ → X) (l: X) := ∀ ε > 0, ∀ N : ℕ, ∃ p ≥ N, d (x p) l < ε
 
-def complete := ∀ x : ℕ → X, ∃ l : X, converge x l
+def complete (T: Type) [espace_metrique T] := ∀ x : ℕ → T, cauchy x → ∃ l : T, converge x l
+
+/-- bornee => bounded (range x) -/
+lemma bornee_est_bounded (x: ℕ → X): bornee x → ∃ M > 0, bounded (λ x y, d x y ≤ M) (range x) :=
+begin
+intro Hb,
+have h: set.nonempty (range x) := range_nonempty_iff_nonempty.2 nonempty_of_inhabited,
+obtain ⟨ y, hy ⟩ := h,
+obtain ⟨ M, ⟨ hm, hbdd⟩ ⟩ := Hb y,
+use M,
+split,
+exact hm,
+use y,
+intros x hx,
+simp at hx,
+obtain ⟨ n, hn ⟩ := hx,
+rw ← hn,
+exact hbdd n,
+end
+
+/-- bounded (range x) => bornee -/
+-- niveau: moyen
+lemma bounded_est_bornee (x: ℕ → X): ∃ M > 0, bounded (λ x y, d x y ≤ M) (range x) → bornee x := sorry
 
 /-- Un exemple de preuve --
 -- Toute suite convergente est de cauchy --
@@ -126,22 +157,50 @@ end
 -- et il faut prouver que R est séquentiellement compact, ce qui reste à peu près la même galère pour R.
 -- mais devient intéressant si on veut introduire la complétion d'un espace.
 
-lemma sup_est_optimal (S: set ℝ) : ∀ ε > 0, ∃ x ∈ S, (real.Sup S) - ε < x := sorry
-lemma inf_est_optimal (S: set ℝ) : ∀ ε > 0, ∃ x ∈ S, x < (real.Inf S) + ε := sorry
+def strictement_croissante [lo: linear_order X] (x: ℕ → X) := ∀ p : ℕ, ∀ q > p, x p < x q
 
-lemma lemme_fondateur_de_bw [lo: linear_order X] (S: set X) 
+-- on a pas besoin d'un ordre complet linéaire… mais bon.
+-- Note à moi-même (Ryan): quel est la structure d'ordre suffisante pour avoir ce résultat?
+-- il suffit que l'inf existe et soit atteint → (X, ≤) est bien fondé!
+-- construire x_n = inf (S \ { x_i | i < n }) par induction forte.
+lemma construire_suite_strictement_croissante [lo: complete_linear_order X] {S: set X} (Hinf: set.infinite S):
+  (∀ M ⊂ S, M.nonempty → (Inf M ∈ M)) → ∃ x : ℕ → X, strictement_croissante x ∧ (range x) ⊂ S := sorry
+
+-- preuve un peu moche, à embellir?
+-- proposition: prendre la contraposition plutôt que l'absurde et pour la preuve interne, faire du direct.
+lemma lemme_fondateur_de_bw [lo: complete_linear_order X] (S: set X) 
   -- si pour toute partie M non vide de S, inf(M), sup(M) existent et sont dans M.
-  (H: ∀ M ⊂ S, set.nonempty M → (is_well_order X lo.lt) ∧ (is_well_order X (flip lo.lt))): set.finite S :=
+  (H: ∀ U ⊂ S, U.nonempty → complete_lattice.Sup U ∈ U ∧ complete_lattice.Inf U ∈ U): set.finite S :=
 begin
 by_contra,
 -- en supposant S infini, on peut construire une infinité de x_n comme il suit:
--- construire x_n = inf (S \ { x_i | i < n }) dans S avec S = (x_i)_(i dans I), I ensemble d'ordinaux
--- prendre X = { x_n | n ≥ 0 } partie de S non vide
--- puisque (x_n)_n est une suite infine strictement croissante, alors sup(X) n'est pas dans X
--- or, X est une partie de S, par caractère bien fondé, c'est absurde.
--- donc S est fini.
--- ça semble requérir fortement ZFC.
-sorry
+suffices hsuite: ∃ x : ℕ → X, strictement_croissante x ∧ (range x) ⊂ S, from begin
+  -- prendre X = { x_n | n ≥ 0 } partie de S non vide
+  -- puisque (x_n)_n est une suite infine strictement croissante, alors sup(X) n'est pas dans X
+  -- or, X est une partie de S, par caractère bien fondé, c'est absurde.
+  -- donc S est fini.
+  obtain ⟨ x, hm, R1 ⟩ := hsuite,
+  have R2: (range x).nonempty := range_nonempty_iff_nonempty.2 nonempty_of_inhabited,
+  have: ¬((complete_lattice.Sup (range x)) ∈ (range x)) := begin
+    by_contra,
+    simp at a_1,
+    -- puisque a_1 donne le fait que le sup (range x) est dans (range x)
+    -- il existe donc n0 tel que x n0 = sup (range x)
+    obtain ⟨ n₀, h ⟩ := a_1,
+    -- or: x (n0 + 1) > x n0 = sup (range x).
+    have : x (n₀ + 1) > x n₀ := begin
+      apply hm,
+      exact lt_add_one n₀,
+    end,
+    -- absurde par déf du sup (linarith ne suffira pas.)
+    -- introduire l'inégalité du sup pour x (n0 + 1), réécrire le sup avec x n0.
+    -- conclure avec linarith.
+    sorry -- niveau: très facile
+  end,
+  apply this,
+  exact (H (range x) R1 R2).1,
+end,
+exact construire_suite_strictement_croissante a (λ M hs hn, (H M hs hn).2),
 end
 
 lemma bolzano_weierstrass_v2 (S: set ℝ): (set.infinite S) → bdd_above S ∧ bdd_below S → ∃ l : ℝ, point_limite S l :=
@@ -150,9 +209,13 @@ intros hs hb,
 by_contra,
 push_neg at a,
 apply hs,
+-- TODO: se démerder avec ces histoires de réseaux complets…
+-- Note à moi-même (Ryan): partir d'un conditionnally complete lattice, le compléter par hypothèse sur les bornes
+-- puis le filer à lemme_fondateur_de_bw (?).
 apply lemme_fondateur_de_bw,
 intros M subM hM,
 split,
+-- niveau: difficile
 -- prouver que inf(M) et sup(M) existent (bornitude de S).
 -- inf(M) est dans M puisque sinon,
 -- par caractérisation séquentielle de l'inf, inf(M) serait un point limite.
@@ -162,29 +225,63 @@ sorry,
 sorry
 end
 
+-- le merveilleux.
+-- niveau: magnifique.
+
+lemma principe_des_tiroirs {A: Type} {B: Type} {f: A → B} (Hinfinite: infinite A) (Hfinite: (range f).finite):
+  ∃ x ∈ (range f), (preimage f {x}).infinite := sorry
+
 lemma bolzano_weierstrass {x: ℕ → ℝ}: bornee x → ∃ l : ℝ, adhere x l :=
 begin
 intro Hb,
 have bdd_above_and_below_of_image: bdd_above (range x) ∧ bdd_below (range x) := sorry,
-by_cases (set.infinite (range x)),
-obtain ⟨ l ⟩ := bolzano_weierstrass_v2 (range x) h bdd_above_and_below_of_image,
-use l,
-obtain ⟨ y ⟩ := h_1,
-sorry,
--- cas fini trivial: x est non-injective de ℕ dans S.
--- par principe des tiroirs, il existe x_0 dans S tel que x^-1{x_0} est infini.
--- ainsi, pour tout eps > 0, pour tout N >= 0, x^-1{x_0} \ [[0, N - 1]] est infini, donc on en prend n dedans, alors x_n = x_0.
--- d'où le résultat désiré d'adhérence.
-sorry,
+by_cases (set.finite (range x)),
+{
+  -- par principe des tiroirs, il existe x_0 dans S tel que x^-1{x_0} est infini.
+  obtain ⟨ l, hl, hpreimage ⟩  := principe_des_tiroirs nat.infinite h,
+  use l,
+  intros ε hε N,
+  -- niveau: moyen.
+  -- prendre p = min (x^-1{x_0} \ [[0, N - 1]]) (non vide & partie de N & infini).
+  -- un tel p vérifie x p = l, donc d (x p) l = 0 < ε.
+  -- d'où le résultat désiré d'adhérence.
+  sorry,
+},
+{
+  obtain ⟨ l ⟩ := bolzano_weierstrass_v2 (range x) h bdd_above_and_below_of_image,
+  use l,
+  obtain ⟨ y, ⟨ hssuite, hcv ⟩ ⟩ := h_1,
+  intros ε hε N,
+  obtain ⟨ N₀, hcv ⟩ := hcv ε hε,
+  suffices hX: ∃ n₀ ≥ N, ∃ p₀ ≥ N₀, x n₀ = y p₀, begin
+    obtain ⟨ n₀, Hn0, p₀, Hpn0, Hxy ⟩ := hX,
+    use n₀,
+    split,
+    exact Hn0,
+    have := hcv p₀ Hpn0,
+    rw ← Hxy at this,
+    rw espace_metrique.sym at this,
+    exact this,
+  end,
+  -- il suffit de prouver qu'on peut trouver n0 assez grand et p0 assez grand tel que x n0 = y p0
+  -- c'est suffisant de prouver que par infinitude, on peut trouver p0 assez grand tel que y p0 possède un rang assez grand.
+  -- niveau: moyen.
+  -- preuve: prouver que la préimage de x par { y(p) | p ≥ N0} est infinie, c'est vrai car son complémentaire est finie (immédiatement).
+  -- donc, on prend un n0 plus grand que N dedans, possible par définition de l'infinitude.
+  sorry,
+}
 end
 
+-- niveau: facile
 lemma cauchy_est_bornee {x: ℕ → X} : cauchy x → bornee x := sorry
+-- niveau: moyen
 lemma cauchy_admet_une_va {x: ℕ → X} : cauchy x → ∀ l₁ : X, ∀ l₂ : X, adhere x l₁ ∧ adhere x l₂ → l₁ = l₂ := sorry
+-- niveau: difficile
 lemma unicite_de_la_va {x: ℕ → X} {l: X} : adhere x l → (∀ l₀ : X, adhere x l₀ →  l = l₀) → converge x l := sorry
 
-theorem R_is_complete (x : ℕ → ℝ) : cauchy x → ∃ l : ℝ, converge x l:=
+theorem R_is_complete : complete ℝ :=
 begin
- intros c,
+ intros x c,
  have := bolzano_weierstrass (cauchy_est_bornee c),
  obtain ⟨ l ⟩ := this,
  use l,
